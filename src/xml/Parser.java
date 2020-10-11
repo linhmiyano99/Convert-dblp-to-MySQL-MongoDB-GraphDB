f4e8c0a29214ca16a29a5b40937ee64c76b90d0e
ntmlinh <17520688@gm.uit.edu.vn>
2020-10-11 22:00:01 +0700

    for mongo db

--- src/xml/Parser.java
+++ src/xml/Parser.java
@@ -10,9 +10,21 @@ import org.xml.sax.*;
 import org.xml.sax.helpers.*;
 
 import database.DBConnection;
+import com.mongodb.*;
+import java.util.List;
+import java.util.ArrayList;
+
+
+
 
 public class Parser {
 	private Connection conn;
+	MongoClient mongoClient;
+	 DB db;
+	 DBCollection dbPaper;
+     DBCollection dbAuthor;
+     DBCollection dbCitation;
+     DBCollection dbConference; 
 	private int curElement = -1;
 	private int ancestor = -1;
 	private Paper paper;
@@ -25,7 +37,7 @@ public class Parser {
 	public static final int GRAPH = 1;
 	public static final int MYSQL = 2;
 	
-	private class ConfigHandler extends DefaultHandler {
+	private class ConfigMySQLHandler extends DefaultHandler {
 		public void startElement(String namespaceURI, String localName,
 				String rawName, Attributes atts) throws SAXException {
 			if (rawName.equals("inproceedings")) {
@@ -153,36 +165,195 @@ public class Parser {
 				}
 			}
 		}
-
-		private void Message(String mode, SAXParseException exception) {
-			System.out.println(mode + " Line: " + exception.getLineNumber()
-					+ " URI: " + exception.getSystemId() + "\n" + " Message: "
-					+ exception.getMessage());
-		}
-
-		public void warning(SAXParseException exception) throws SAXException {
-			Message("**Parsing Warning**\n", exception);
-			throw new SAXException("Warning encountered");
-		}
-
-		public void error(SAXParseException exception) throws SAXException {
-			Message("**Parsing Error**\n", exception);
-			throw new SAXException("Error encountered");
-		}
-
-		public void fatalError(SAXParseException exception) throws SAXException {
-			Message("**Parsing Fatal Error**\n", exception);
-			throw new SAXException("Fatal Error encountered");
-		}
+		
 	}
+	private class ConfigMongoDBHandler extends DefaultHandler {
+		public void startElement(String namespaceURI, String localName,
+				String rawName, Attributes atts) throws SAXException {
+			if (rawName.equals("inproceedings")) {
+				ancestor = Element.INPROCEEDING;
+				curElement = Paper.INPROCEEDING;
+				paper = new Paper();
+				paper.key = atts.getValue("key");
+			} else if (rawName.equals("proceedings")) {
+				ancestor = Element.PROCEEDING;
+				curElement = Conference.PROCEEDING;
+				conf = new Conference();
+				conf.key = atts.getValue("key");
+			} else if (rawName.equals("author") && ancestor == Element.INPROCEEDING) {
+				author = new StringBuffer();
+			}
+
+			if (ancestor == Element.INPROCEEDING) {
+				curElement = Paper.getElement(rawName);
+			} else if (ancestor == Element.PROCEEDING) {
+				curElement = Conference.getElement(rawName);
+			} else if (ancestor == -1) {
+				ancestor = Element.OTHER;
+				curElement = Element.OTHER;
+			} else {
+				curElement = Element.OTHER;
+			}
+
+			line++;
+		}
+
+		public void characters(char[] ch, int start, int length)
+				throws SAXException {
+			if (ancestor == Element.INPROCEEDING) {
+				String str = new String(ch, start, length).trim();
+				if (curElement == Paper.AUTHOR) {
+					author.append(str);
+				} else if (curElement == Paper.CITE) {
+					paper.citations.add(str);
+				} else if (curElement == Paper.CONFERENCE) {
+					paper.conference = str;
+				} else if (curElement == Paper.TITLE) {
+					paper.title += str;
+				} else if (curElement == Paper.YEAR) {
+					paper.year = Integer.parseInt(str);
+				}
+			} else if (ancestor == Element.PROCEEDING) {
+				String str = new String(ch, start, length).trim();
+				if (curElement == Conference.CONFNAME) {
+					conf.name = str;
+				} else if (curElement == Conference.CONFDETAIL) {
+					conf.detail = str;
+				}
+			}
+		}
+
+		public void endElement(String namespaceURI, String localName,
+				String rawName) throws SAXException {
+			List<DBObject> listPapers = new ArrayList<>();
+			List<DBObject> listAuthors = new ArrayList<>();
+			List<DBObject> listCitations = new ArrayList<>();
+			List<DBObject> listConferences = new ArrayList<>();
+			if (rawName.equals("author") && ancestor == Element.INPROCEEDING) {
+				paper.authors.add(author.toString().trim());
+			}
+		
+
+			if (Element.getElement(rawName) == Element.INPROCEEDING) {
+				ancestor = -1;
+				if (paper.title.equals("") || paper.conference.equals("")
+						|| paper.year == 0) {
+					System.out.println("Error in parsing " + paper);
+					errors++;
+					return;
+				}
+				DBObject documentPaper = new BasicDBObject("paper", new BasicDBObject("title", paper.title)
+				           .append("year", paper.year)
+				           .append("conference", paper.conference)
+				           .append("paper_key", paper.key));
+				    listPapers.add(documentPaper);
+
+
+				for (String author: paper.authors) {
+					DBObject documentAuthor = new BasicDBObject("author", new BasicDBObject("name", author)
+				               .append("paper_key", paper.key));
+				    listAuthors.add(documentAuthor);
+
+					
+				}
+				for (String cited: paper.citations) {
+					if (!cited.equals("...")) {
+						DBObject documentCitation = new BasicDBObject("citation", new BasicDBObject("paper_cite_key", paper.key)
+				                   .append("paper_cited_key", cited));
+						listCitations.add(documentCitation);
+					}
+				}
+			} else if (Element.getElement(rawName) == Element.PROCEEDING) {
+				ancestor = -1;
+				if (conf.name.equals(""))
+					conf.name = conf.detail;
+				if (conf.key.equals("") || conf.name.equals("")
+						|| conf.detail.equals("")) {
+					System.out.println("Line:" + line);
+					System.exit(0);
+				}
+				DBObject documentConferences = new BasicDBObject("citation", new BasicDBObject("conf_key", conf.key)
+				           .append("name",  conf.name)
+				           .append("detail", conf.detail));
+				listConferences.add(documentConferences);
+			}
+			if (line % 10000 == 0) {
+					try{
+				dbPaper.insert(listPapers);
+				listPapers.clear();
+					}
+					catch(Exception e) {
+						e.printStackTrace();
+
+					}
+				
+					try{
+					dbAuthor.insert(listAuthors);
+					listAuthors.clear();
+
+					}
+					catch(Exception e) {
+						e.printStackTrace();
+
+					}
+			
+					try{
+
+					dbCitation.insert(listCitations);
+					listCitations.clear();
+
+				}
+				catch(Exception e) {
+					e.printStackTrace();
+
+				}
+				
+					try{
+
+					dbConference.insert(listConferences);
+					listConferences.clear();
+
+				}
+				catch(Exception e) {
+					e.printStackTrace();
+
+				}
+			
+				
+			}
+		
+		}
+
+
+	private void Message(String mode, SAXParseException exception) {
+		System.out.println(mode + " Line: " + exception.getLineNumber()
+				+ " URI: " + exception.getSystemId() + "\n" + " Message: "
+				+ exception.getMessage());
+	}
+
+	public void warning(SAXParseException exception) throws SAXException {
+		Message("**Parsing Warning**\n", exception);
+		throw new SAXException("Warning encountered");
+	}
+
+	public void error(SAXParseException exception) throws SAXException {
+		Message("**Parsing Error**\n", exception);
+		throw new SAXException("Error encountered");
+	}
+
+	public void fatalError(SAXParseException exception) throws SAXException {
+		Message("**Parsing Fatal Error**\n", exception);
+		throw new SAXException("Fatal Error encountered");
+	}
+}
 
 	Parser(String uri, int db) throws SQLException {
 		switch(db) {
 		case Parser.MONGODB:
-			parserGraph(uri);
+			parserMongoDB(uri);
 			break;
 		case Parser.GRAPH:
-			parserMongoDB(uri);
+			parserGraph(uri);
 			break;
 		case Parser.MYSQL:
 			parserMySQL(uri);
@@ -209,7 +380,7 @@ public class Parser {
 
 			SAXParserFactory parserFactory = SAXParserFactory.newInstance();
 			SAXParser parser = parserFactory.newSAXParser();
-			ConfigHandler handler = new ConfigHandler();
+			ConfigMySQLHandler handler = new ConfigMySQLHandler();
 			parser.getXMLReader().setFeature(
 					"http://xml.org/sax/features/validation", true);
 			parser.parse(new File(uri), handler);
@@ -237,49 +408,35 @@ public class Parser {
 	}
 	
 	public void parserMongoDB(String uri) throws SQLException {
-		try {
-			System.out.println("Parsing...");
-			conn = DBConnection.getMongoDBConnection();
-			conn.setAutoCommit(false);
-			stmt_inproc = conn
-					.prepareStatement("insert into paper(title,year,conference,paper_key) values (?,?,?,?)");
+		try{
 
-			stmt_author = conn
-					.prepareStatement("insert into author(name,paper_key) values (?,?)");
+            // To connect to mongodb server
+             mongoClient = new MongoClient( "localhost" , 27017);
 
-			stmt_cite = conn
-					.prepareStatement("insert into citation(paper_cite_key,paper_cited_key) values (?,?)");
+            // Now connect to your databases
+            db = mongoClient.getDB("dblp");
+            System.out.println("Connect to database successfully");
 
-			stmt_conf = conn
-					.prepareStatement("insert into conference(conf_key,name,detail) values (?,?,?)");
+            // if collection doesn't exists, MongoDB will create it     
+            dbPaper = db.getCollection("paper");
+            dbAuthor = db.getCollection("author");
+            dbCitation = db.getCollection("citation");
+            dbConference = db.getCollection("conference");
+
+            System.out.println("Collection coll selected successfully");
+
 
 			SAXParserFactory parserFactory = SAXParserFactory.newInstance();
 			SAXParser parser = parserFactory.newSAXParser();
-			ConfigHandler handler = new ConfigHandler();
+			ConfigMongoDBHandler handler = new ConfigMongoDBHandler();
 			parser.getXMLReader().setFeature(
 					"http://xml.org/sax/features/validation", true);
 			parser.parse(new File(uri), handler);
-			
-			try {
-				stmt_inproc.executeBatch();
-				stmt_conf.executeBatch();
-				stmt_author.executeBatch();
-				stmt_cite.executeBatch();
-				conn.commit();
-				System.out.println("Processed " + line);
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			conn.close();
-			System.out.println("num of errors: " + errors);
-		} catch (IOException e) {
-			System.out.println("Error reading URI: " + e.getMessage());
-		} catch (SAXException e) {
-			System.out.println("Error in parsing: " + e.getMessage());
-		} catch (ParserConfigurationException e) {
-			System.out.println("Error in XML parser configuration: "
-					+ e.getMessage());
-		}
+			 
+            System.out.println("Document added to Collection coll successfully");
+		 }catch(Exception e){
+             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+          }
 	}
 	
 	public  void parserGraph(String uri) throws SQLException {
@@ -301,7 +458,7 @@ public class Parser {
 
 			SAXParserFactory parserFactory = SAXParserFactory.newInstance();
 			SAXParser parser = parserFactory.newSAXParser();
-			ConfigHandler handler = new ConfigHandler();
+			ConfigMySQLHandler handler = new ConfigMySQLHandler();
 			parser.getXMLReader().setFeature(
 					"http://xml.org/sax/features/validation", true);
 			parser.parse(new File(uri), handler);
@@ -331,7 +488,7 @@ public class Parser {
 	public static void main(String[] args) throws SQLException, ClassNotFoundException{
 		Long start = System.currentTimeMillis();
 		System.out.println(start);
-		Parser p = new Parser("C:/Users/17520/Documents/GitHub/DBLP-PARSER/dblp/dblp.xml", MYSQL);
+		Parser p = new Parser("C:/Users/17520/Documents/GitHub/DBLP-PARSER/dblp.xml", MONGODB);
 		Long end = System.currentTimeMillis();
 		System.out.println("Used:" + (end - start) / 1000 + "second");
 		
