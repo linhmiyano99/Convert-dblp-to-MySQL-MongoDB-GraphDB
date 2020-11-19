24e81b09659c87dfea40280871b657474e66c5f7
ntmlinh <hoadaomylinh@gmail.com>
2020-11-19 10:52:55 +0700

    update graph

--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -1,16 +1,20 @@
 package database;
 
 import java.sql.*;
+
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Driver;
  
 public class DBConnection {
 	private static String dbMySQLUrl = "jdbc:mysql://localhost:3306/dblp?characterEncoding=latin1&useConfigs=maxPerformance";
-	private static String username = "root";
+	private static String dbNeo4jLUrl = "bolt://localhost:7687/dblp";
 	private static String password = "123456";
 	
 	public static Connection getMySQLConnection() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			return DriverManager.getConnection(dbMySQLUrl,username, password);
+			return DriverManager.getConnection(dbMySQLUrl,"root", password);
 		}
 		catch (Exception e){
 			System.out.println("Error while opening a conneciton to database server: "
@@ -20,24 +24,13 @@ public class DBConnection {
 
 	}
 
-	public static Connection getMongoDBConnection() {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			
-			return DriverManager.getConnection(dbMySQLUrl,username, password);
-		}
-		catch (Exception e){
-			System.out.println("Error while opening a conneciton to database server: "
-					+ e.getMessage());
-		}
-		return null;
 
-	}
-
-	public static Connection getGraphConnection() {
+	public static Driver getNeo4jConnection() {
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			return DriverManager.getConnection(dbMySQLUrl,username, password);
+			Class.forName("org.neo4j.jdbc.Driver");
+			System.out.println("good");
+
+			return (Driver) GraphDatabase.driver(dbNeo4jLUrl,AuthTokens.basic("neo4j", password));
 		}
 		catch (Exception e){
 			System.out.println("Error while opening a conneciton to database server: "
--- src/xml/Parser.java
+++ src/xml/Parser.java
@@ -6,13 +6,14 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import javax.xml.parsers.*;
+
+import org.neo4j.driver.Session;
+import org.neo4j.driver.Driver;
 import org.xml.sax.*;
 import org.xml.sax.helpers.*;
 
 import database.DBConnection;
 import com.mongodb.*;
-import java.util.List;
-import java.util.ArrayList;
 
 
 
@@ -20,11 +21,12 @@ import java.util.ArrayList;
 public class Parser {
 	private Connection conn;
 	MongoClient mongoClient;
-	 DB db;
-	 DBCollection dbPaper;
-     DBCollection dbAuthor;
-     DBCollection dbCitation;
-     DBCollection dbConference; 
+	DB db;
+	DBCollection dbPaper;
+    DBCollection dbAuthor;
+    DBCollection dbCitation;
+    DBCollection dbConference; 
+    Driver driver;
 	private int curElement = -1;
 	private int ancestor = -1;
 	private Paper paper;
@@ -34,7 +36,7 @@ public class Parser {
 	int errors = 0;
 	StringBuffer author;
 	public static final int MONGODB = 0;
-	public static final int GRAPH = 1;
+	public static final int NEO4J = 1;
 	public static final int MYSQL = 2;
 	
 	private class ConfigMySQLHandler extends DefaultHandler {
@@ -144,6 +146,8 @@ public class Parser {
 					stmt_conf.setString(1, conf.key);
 					stmt_conf.setString(2, conf.name);
 					stmt_conf.setString(3, conf.detail);
+			        System.out.print(conf.name);
+
 					stmt_conf.addBatch();
 				} catch (SQLException e) {
 					e.printStackTrace();
@@ -159,7 +163,7 @@ public class Parser {
 					stmt_author.executeBatch();
 					stmt_cite.executeBatch();
 					conn.commit();
-					// System.out.println("Processing " + line);
+					 System.out.println("Processing " + line);
 				} catch (SQLException e) {
 					System.err.println(e.getMessage());
 				}
@@ -167,6 +171,143 @@ public class Parser {
 		}
 		
 	}
+		private class ConfigNeo4jHandler extends DefaultHandler {
+			public void startElement(String namespaceURI, String localName,
+					String rawName, Attributes atts) throws SAXException {
+				if (rawName.equals("inproceedings")) {
+					ancestor = Element.INPROCEEDING;
+					curElement = Paper.INPROCEEDING;
+					paper = new Paper();
+					paper.key = atts.getValue("key");
+				} else if (rawName.equals("proceedings")) {
+					ancestor = Element.PROCEEDING;
+					curElement = Conference.PROCEEDING;
+					conf = new Conference();
+					conf.key = atts.getValue("key");
+				} else if (rawName.equals("author") && ancestor == Element.INPROCEEDING) {
+					author = new StringBuffer();
+				}
+
+				if (ancestor == Element.INPROCEEDING) {
+					curElement = Paper.getElement(rawName);
+				} else if (ancestor == Element.PROCEEDING) {
+					curElement = Conference.getElement(rawName);
+				} else if (ancestor == -1) {
+					ancestor = Element.OTHER;
+					curElement = Element.OTHER;
+				} else {
+					curElement = Element.OTHER;
+				}
+
+				line++;
+			}
+
+			public void characters(char[] ch, int start, int length)
+					throws SAXException {
+				if (ancestor == Element.INPROCEEDING) {
+					String str = new String(ch, start, length).trim();
+					if (curElement == Paper.AUTHOR) {
+						author.append(str);
+					} else if (curElement == Paper.CITE) {
+						paper.citations.add(str);
+					} else if (curElement == Paper.CONFERENCE) {
+						paper.conference = str;
+					} else if (curElement == Paper.TITLE) {
+						paper.title += str;
+					} else if (curElement == Paper.YEAR) {
+						paper.year = Integer.parseInt(str);
+					}
+				} else if (ancestor == Element.PROCEEDING) {
+					String str = new String(ch, start, length).trim();
+					if (curElement == Conference.CONFNAME) {
+						conf.name = str;
+					} else if (curElement == Conference.CONFDETAIL) {
+						conf.detail = str;
+					}
+				}
+			}
+
+			public void endElement(String namespaceURI, String localName,
+					String rawName) throws SAXException {
+				Session session = driver.session();
+				if (rawName.equals("author") && ancestor == Element.INPROCEEDING) {
+					paper.authors.add(author.toString().trim());
+				}
+			
+
+				if (Element.getElement(rawName) == Element.INPROCEEDING) {
+					ancestor = -1;
+					if (paper.title.equals("") || paper.conference.equals("")
+							|| paper.year == 0) {
+						System.out.println("Error in parsing " + paper);
+						errors++;
+						return;
+					}
+					if(paper.title.contains("\"")) {
+						paper.title = paper.title.replace("\"", "\\\"");
+					}
+				    session.writeTransaction(
+			        		tx -> tx.run("MERGE (a:paper {title: \""+ paper.title 
+			        				+ "\", year: "+paper.year
+			        				+", conference: \""+paper.conference 
+			        				+"\",paper_key: \""+paper.key+"\"})"));
+			        
+
+
+
+					for (String author: paper.authors) {
+						session.writeTransaction(
+				        		tx -> tx.run("MERGE (b:author {name: \""+author+"\",paper_key: \""+paper.key+"\"})"));
+				       
+
+						
+					}
+
+					for (String cited: paper.citations) {
+						if (!cited.equals("...")) {
+							 session.writeTransaction(
+						        		tx -> tx.run("MERGE (c:citation {paper_cite_key: \""+paper.key+"\",paper_cited_key: \""+cited+"\"})"));
+						}
+					}
+				} else if (Element.getElement(rawName) == Element.PROCEEDING) {
+					ancestor = -1;
+					if (conf.name.equals(""))
+						conf.name = conf.detail;
+					if (conf.key.equals("") || conf.name.equals("")
+							|| conf.detail.equals("")) {
+						System.out.println("Line:" + line);
+						System.exit(0);
+					}
+					session.writeTransaction(
+			        		tx -> tx.run("MERGE (d:conference {conf_key: \""+conf.key+"\",name: \""+conf.name+"\",detail: \""+conf.detail+"\"})"));
+
+				}
+				session.close();
+			}
+
+
+		private void Message(String mode, SAXParseException exception) {
+			System.out.println(mode + " Line: " + exception.getLineNumber()
+					+ " URI: " + exception.getSystemId() + "\n" + " Message: "
+					+ exception.getMessage());
+		}
+
+		public void warning(SAXParseException exception) throws SAXException {
+			Message("**Parsing Warning**\n", exception);
+			throw new SAXException("Warning encountered");
+		}
+
+		public void error(SAXParseException exception) throws SAXException {
+			Message("**Parsing Error**\n", exception);
+			throw new SAXException("Error encountered");
+		}
+
+		public void fatalError(SAXParseException exception) throws SAXException {
+			Message("**Parsing Fatal Error**\n", exception);
+			throw new SAXException("Fatal Error encountered");
+		}
+	}
+
 	private class ConfigMongoDBHandler extends DefaultHandler {
 		public void startElement(String namespaceURI, String localName,
 				String rawName, Attributes atts) throws SAXException {
@@ -225,10 +366,6 @@ public class Parser {
 
 		public void endElement(String namespaceURI, String localName,
 				String rawName) throws SAXException {
-			List<DBObject> listPapers = new ArrayList<>();
-			List<DBObject> listAuthors = new ArrayList<>();
-			List<DBObject> listCitations = new ArrayList<>();
-			List<DBObject> listConferences = new ArrayList<>();
 			if (rawName.equals("author") && ancestor == Element.INPROCEEDING) {
 				paper.authors.add(author.toString().trim());
 			}
@@ -273,43 +410,12 @@ public class Parser {
 					System.out.println("Line:" + line);
 					System.exit(0);
 				}
-				DBObject documentConferences = new BasicDBObject("citation", new BasicDBObject("conf_key", conf.key)
+				DBObject documentConferences = new BasicDBObject("conference", new BasicDBObject("conf_key", conf.key)
 				           .append("name",  conf.name)
 				           .append("detail", conf.detail));
 				dbConference.insert(documentConferences);
 
 			}
-			/*
-			 * if (line % 1000 == 0) { try{ dbPaper.insert(listPapers); } catch(Exception e)
-			 * { e.printStackTrace();
-			 * 
-			 * }
-			 * 
-			 * try{ dbAuthor.insert(listAuthors);
-			 * 
-			 * } catch(Exception e) { e.printStackTrace();
-			 * 
-			 * }
-			 * 
-			 * try{
-			 * 
-			 * dbCitation.insert(listCitations);
-			 * 
-			 * } catch(Exception e) { e.printStackTrace();
-			 * 
-			 * }
-			 * 
-			 * try{
-			 * 
-			 * dbConference.insert(listConferences);
-			 * 
-			 * } catch(Exception e) { e.printStackTrace();
-			 * 
-			 * }
-			 * 
-			 * 
-			 * }
-			 */
 		
 		}
 
@@ -341,7 +447,7 @@ public class Parser {
 		case Parser.MONGODB:
 			parserMongoDB(uri);
 			break;
-		case Parser.GRAPH:
+		case Parser.NEO4J:
 			parserGraph(uri);
 			break;
 		case Parser.MYSQL:
@@ -375,10 +481,11 @@ public class Parser {
 			parser.parse(new File(uri), handler);
 			
 			try {
-				stmt_inproc.executeBatch();
-				stmt_conf.executeBatch();
-				stmt_author.executeBatch();
-				stmt_cite.executeBatch();
+				
+			
+				  stmt_inproc.executeBatch(); stmt_conf.executeBatch();
+				  stmt_author.executeBatch(); stmt_cite.executeBatch();
+				
 				conn.commit();
 				System.out.println("Processed " + line);
 			} catch (SQLException e) {
@@ -396,6 +503,7 @@ public class Parser {
 		}
 	}
 	
+	@SuppressWarnings("deprecation")
 	public void parserMongoDB(String uri) throws SQLException {
 		try{
 
@@ -414,7 +522,6 @@ public class Parser {
 
             System.out.println("Collection coll selected successfully");
 
-
 			SAXParserFactory parserFactory = SAXParserFactory.newInstance();
 			SAXParser parser = parserFactory.newSAXParser();
 			ConfigMongoDBHandler handler = new ConfigMongoDBHandler();
@@ -428,56 +535,40 @@ public class Parser {
           }
 	}
 	
-	public  void parserGraph(String uri) throws SQLException {
+	public  void parserGraph(String uri)  throws SQLException {
 		try {
-			System.out.println("Parsing...");
-			conn = DBConnection.getGraphConnection();
-			conn.setAutoCommit(false);
-			stmt_inproc = conn
-					.prepareStatement("insert into paper(title,year,conference,paper_key) values (?,?,?,?)");
-
-			stmt_author = conn
-					.prepareStatement("insert into author(name,paper_key) values (?,?)");
-
-			stmt_cite = conn
-					.prepareStatement("insert into citation(paper_cite_key,paper_cited_key) values (?,?)");
-
-			stmt_conf = conn
-					.prepareStatement("insert into conference(conf_key,name,detail) values (?,?,?)");
-
-			SAXParserFactory parserFactory = SAXParserFactory.newInstance();
-			SAXParser parser = parserFactory.newSAXParser();
-			ConfigMySQLHandler handler = new ConfigMySQLHandler();
-			parser.getXMLReader().setFeature(
-					"http://xml.org/sax/features/validation", true);
-			parser.parse(new File(uri), handler);
-			
-			try {
-				stmt_inproc.executeBatch();
-				stmt_conf.executeBatch();
-				stmt_author.executeBatch();
-				stmt_cite.executeBatch();
-				conn.commit();
-				System.out.println("Processed " + line);
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			conn.close();
-			System.out.println("num of errors: " + errors);
-		} catch (IOException e) {
-			System.out.println("Error reading URI: " + e.getMessage());
-		} catch (SAXException e) {
-			System.out.println("Error in parsing: " + e.getMessage());
-		} catch (ParserConfigurationException e) {
-			System.out.println("Error in XML parser configuration: "
-					+ e.getMessage());
-		}
+		System.out.println("Parsing...");
+		driver = (Driver) DBConnection.getNeo4jConnection();
+		//title,year,conference,paper_key
+		//author(name,paper_key)
+		//citation(paper_cite_key,paper_cited_key)
+		//conference(conf_key,name,detail)
+		
+    
+       
+		SAXParserFactory parserFactory = SAXParserFactory.newInstance();
+		SAXParser parser = parserFactory.newSAXParser();
+		ConfigNeo4jHandler handler = new ConfigNeo4jHandler();
+		parser.getXMLReader().setFeature(
+				"http://xml.org/sax/features/validation", true);
+		parser.parse(new File(uri), handler);
+		driver.close();
+		System.out.println("num of errors: " + errors);
+	} catch (IOException e) {
+		System.out.println("Error reading URI: " + e.getMessage());
+	} catch (SAXException e) {
+		System.out.println("Error in parsing: " + e.getMessage());
+	} catch (ParserConfigurationException e) {
+		System.out.println("Error in XML parser configuration: "
+				+ e.getMessage());
 	}
+	    }
+	
 	
 	public static void main(String[] args) throws SQLException, ClassNotFoundException{
 		Long start = System.currentTimeMillis();
 		System.out.println(start);
-		Parser p = new Parser("C:/Users/17520/Documents/GitHub/DBLP-PARSER/dblp.xml", MONGODB);
+		Parser p = new Parser("C:/Users/17520/Documents/GitHub/DBLP-PARSER/dblp.xml", MYSQL);
 		Long end = System.currentTimeMillis();
 		System.out.println("Used:" + (end - start) / 1000 + "second");
 		
